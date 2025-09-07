b2bc1bf8bf6ba53460e0ceb660b6c89336d402e2
Zhenpeng Ge <zhenpeng.ge@qq.com>
2025-09-07 16:10:40 +0000

    code format

--- ssct_common/src/dummy_sensor/dummy_imu_node.cpp
+++ ssct_common/src/dummy_sensor/dummy_imu_node.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Gezp (https://github.com/gezp).
+// Copyright 2025 Gezp (https://github.com/gezp).
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
